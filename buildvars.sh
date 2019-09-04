cb07c863d9df4f766689306d79f00bab8ebbef90
Travis <travis.wheatley@emergentspace.com>
2019-09-04 12:24:11 -0400

    Adding buildvars.sh and modified .travis.yml to use environment variable

---
+++ buildvars.sh
@@ -0,0 +1,4 @@
+# The Travis-CI auto build system tests the psp submodule by building it in the context of the
+# cFS tree. The CFE_BRANCH environment variable is used by the auto-build system to specify
+# which branch of the cFE tree will be used.
+export CFE_BRANCH="task_3015"
