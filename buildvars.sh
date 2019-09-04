a136b41dc379a570096dc1d287ceb6b55dc643e0
Travis <travis.wheatley@emergentspace.com>
2019-09-04 14:49:58 -0400

    Corrected comment typos

--- buildvars.sh
+++ buildvars.sh
@@ -1,9 +1,8 @@
 # The Travis-CI auto build system tests the psp submodule by building it in the context of the
 # cFS tree. The CFE_BRANCH environment variable is used by the auto-build system to specify
-# which branch of the cFE tree will be used.
+# which branch of the cFS tree will be used.
 export CFS_BRANCH="task_3015"
 
-# The auto build system makes a clone of the cFS tree in order to build the applications in
-# order to build the applications. The following variable points to the get repository that
-# is to be cloned.
+# The auto build system makes a clone of the cFS tree in order to build the applications.
+# The following variable points to the get repository that is to be cloned.
 export CFS_GITREPO="https://github.com/travis-r-wheatley/cFS.git"
