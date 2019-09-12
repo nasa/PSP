1bb489b3c1e17ab22eda33405951e4ddb553d21c
Travis <travis.wheatley@emergentspace.com>
2019-09-12 10:49:07 -0400

    Merging branch task_3015

--- buildvars.sh
+++ buildvars.sh
@@ -1,7 +1,7 @@
 # The Travis-CI auto build system tests the psp submodule by building it in the context of the
 # cFS tree. The CFE_BRANCH environment variable is used by the auto-build system to specify
 # which branch of the cFS tree will be used.
-export CFS_BRANCH="task_3015"
+export CFS_BRANCH="master"
 
 # The auto build system makes a clone of the cFS tree in order to build the applications.
 # The following variable points to the get repository that is to be cloned.
