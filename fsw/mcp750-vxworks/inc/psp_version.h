ac13dcd25efea9927ca2eb3912d029be0d27e236
astrogeco <59618057+astrogeco@users.noreply.github.com>
2020-10-13 11:23:33 -0400

    Bump to v1.5.0-rc1+dev19 and update Readme

--- fsw/mcp750-vxworks/inc/psp_version.h
+++ fsw/mcp750-vxworks/inc/psp_version.h
@@ -29,7 +29,7 @@
 /*
  * Development Build Macro Definitions 
  */
-#define CFE_PSP_IMPL_BUILD_NUMBER 14
+#define CFE_PSP_IMPL_BUILD_NUMBER 19
 #define CFE_PSP_IMPL_BUILD_BASELINE "v1.5.0-rc1"
 
 /*
