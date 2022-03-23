4e1570f42108314238b73a0c81178d0524c4b6c0
Jacob Hageman <jacob.hageman@nasa.gov>
2022-03-23 09:48:35 -0600

    Fix #327, Remove old copyright headers

--- unit-test-coverage/ut-stubs/override_inc/unistd.h
+++ unit-test-coverage/ut-stubs/override_inc/unistd.h
@@ -1,13 +1,3 @@
-/*
- *      Copyright (c) 2019, United States government as represented by the
- *      administrator of the National Aeronautics Space Administration.
- *      All rights reserved. This software was created at NASA Goddard
- *      Space Flight Center pursuant to government contracts.
- *
- *      This is governed by the NASA Open Source Agreement and may be used,
- *      distributed and modified only according to the terms of that agreement.
- */
-
 /* PSP coverage stub replacement for unistd.h */
 #ifndef OVERRIDE_UNISTD_H
 #define OVERRIDE_UNISTD_H
