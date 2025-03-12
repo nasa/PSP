4e7e3f1b5caab965460b21a3c6acaaecb64b5e7e
Jose F. Martinez Pedraza <jose.f.martinez-pedraza@nasa.gov>
2025-03-12 20:15:57 +0000

    Fix #449, update PCS_malloc stub PoolSize overflow

--- unit-test-coverage/ut-stubs/src/PCS_stdlib_handlers.c
+++ unit-test-coverage/ut-stubs/src/PCS_stdlib_handlers.c
@@ -89,7 +89,11 @@ void UT_DefaultHandler_PCS_malloc(void *UserObj, UT_EntryKey_t FuncKey, const UT
         PoolStart = (PoolStart + MPOOL_ALIGN - 1) & ~((cpuaddr)MPOOL_ALIGN - 1);
         PoolSize  = PoolEnd - PoolStart;
 
-        if (PoolSize > (MPOOL_ALIGN * 2))
+        /*
+         * Verify PollSize is greater than minimun size and
+         * that PoolStart didn't go past the PoolEnd during alignment.
+         */
+        if (PoolSize > (MPOOL_ALIGN * 2) && PoolStart < PoolEnd)
         {
             Rec       = (struct MPOOL_REC *)PoolStart;
             NextBlock = PoolStart + MPOOL_ALIGN;
